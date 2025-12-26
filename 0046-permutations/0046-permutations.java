2e41a3a61e973ff762cfff566f98ec2ac06ea5c2
hemanthravva <ravvahemanth@gmail.com>
2025-12-26 22:56:27 +0530

    Time: 1 ms (88.25%), Space: 45.7 MB (23.73%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,35 +1,32 @@
 class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans= new ArrayList <>();
-        List<Integer> ds = new ArrayList<>();
 
-        boolean[] frq = new boolean[nums.length];
-        func(nums,ds,ans,frq);
-        return ans ;
+    private void recurPermute(int index, int[] nums, List<List<Integer>> ans) {
 
+        if (index == nums.length) {
+            List<Integer> ds = new ArrayList<>();
+            for (int i = 0; i < nums.length; i++) {
+                ds.add(nums[i]);
+            }
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
 
+        for (int i = index; i < nums.length; i++) {
+            swap(i, index, nums);
+            recurPermute(index + 1, nums, ans);
+            swap(i, index, nums);
+        }
     }
 
+    private void swap(int i, int j, int[] nums) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
 
-    public static void func(int nums[],List<Integer> ds,List<List<Integer>>  ans,boolean frq[]){
-
-
-        if ( ds.size() ==nums.length ){
-            ans.add(new ArrayList<>(ds));
-            return ;
-        }
-
-
-        for ( int i =0;i<nums.length;i++){
-            if (!frq[i]){
-                frq[i]=true;
-
-                ds.add(nums[i]);
-                func(nums,ds,ans,frq);
-                ds.remove(ds.size() -1);
-
-                frq[i]=false;
-            }
-        }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        recurPermute(0, nums, ans);
+        return ans;
     }
 }
