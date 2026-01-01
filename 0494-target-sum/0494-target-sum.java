d8669b67ec529a3e131747ce2cd0b9d7973a2615
hemanthravva <ravvahemanth@gmail.com>
2026-01-01 21:31:27 +0530

    Time: 656 ms (5.71%), Space: 43 MB (78.48%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,21 +1,16 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        int total = 0;
-        for (int num : nums) total += num;
+        return f(0, 0, nums, target);
+    }
 
-        if (Math.abs(target) > total) return 0;
-        if ((target + total) % 2 != 0) return 0;
-
-        int P = (target + total) / 2;
-
-        int[] dp = new int[P + 1];
-        dp[0] = 1;
-
-        for (int num : nums) {
-            for (int s = P; s >= num; s--) {
-                dp[s] += dp[s - num];
-            }
+    private int f(int i, int sum, int[] nums, int target) {
+        if (i == nums.length) {
+            return sum == target ? 1 : 0;
         }
-        return dp[P];
+
+        int take = f(i + 1, sum + nums[i], nums, target);
+        int notTake = f(i + 1, sum - nums[i], nums, target);
+
+        return take + notTake;
     }
 }
