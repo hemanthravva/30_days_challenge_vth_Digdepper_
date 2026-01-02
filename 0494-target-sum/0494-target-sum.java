b63f5bc5a1e77fa5e28c67f5bbd8e002a124e3cc
hemanthravva <ravvahemanth@gmail.com>
2026-01-02 09:34:58 +0530

    Time: 560 ms (24.36%), Space: 43 MB (78.76%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,28 +1,18 @@
 class Solution {
-    int offset;
-    Integer[][] dp;
-
     public int findTargetSumWays(int[] nums, int target) {
-        int total = 0;
-        for (int x : nums) total += x;
-
-        offset = total;
-        dp = new Integer[nums.length][2 * total + 1];
-
-        return f(0, 0, nums, target);
+       return  f(0,0,nums,target);
     }
 
-    private int f(int i, int sum, int[] nums, int target) {
-        if (i == nums.length) {
+    public static int f(int idx,int sum,int nums[],int target){
+        if (idx == nums.length) {
             return sum == target ? 1 : 0;
         }
 
-        if (dp[i][sum + offset] != null)
-            return dp[i][sum + offset];
+        //pick
+      int take=f(idx+1,sum+nums[idx],nums,target);
+      int not_take=f(idx+1,sum-nums[idx],nums,target);
 
-        int take = f(i + 1, sum + nums[i], nums, target);
-        int notTake = f(i + 1, sum - nums[i], nums, target);
 
-        return dp[i][sum + offset] = take + notTake;
+      return take+not_take;
     }
 }
